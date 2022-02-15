5eddec5c1aa0eb4cf67a186c666cf190fb434c77
Harshil Agarwal <77769646+Encode-pro@users.noreply.github.com>
2022-02-15 15:55:02 +0530

    Pattern

---
+++ Patterns/Rectangle-Pattern.cpp
@@ -0,0 +1,26 @@
+/* Print the following pattern
+    Rows=5, Columns=5
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int row,col;
+    cin>>row>>col;
+    for(int i = 1;i<=row;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
